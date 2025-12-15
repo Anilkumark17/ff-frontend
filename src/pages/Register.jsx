420293f551669eb8978ef8ca865f3aee6f5fc468
Anilkumark17 <anilkumarkondeboina@gmail.com>
2025-12-15 18:35:55 +0530

    changes authentication

---
+++ src/pages/Register.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
